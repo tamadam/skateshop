84b1357c902dea4e67fca2ed7da7b35b0198a951
tamadam <tamadam9@gmail.com>
2024-03-29 11:36:24 +0100

    Add user register API

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,48 @@
+import { signUpFormSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { hash } from "bcrypt";
 import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+
+        const validation = signUpFormSchema.safeParse(body);
+        if (!validation.success) {
+            return NextResponse.json(validation.error.format(), { status: 400 });
+        }
+
+        const { firstName, lastName, email, password } = body;
+
+        const isEmailExist = await prisma.user.findUnique({
+            where: {
+                email
+            },
+        });
+
+        if (isEmailExist) {
+            return NextResponse.json({ user: null, message: "This email is already in use"}, { status: 409 });
+        }
+
+        const hashedPassword = await hash(password, 10);
+        const newUser = await prisma.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword
+            },
+        });
+
+        const { password: newUserPassword, ...newUserWithoutPassword} = newUser;
+
+        return NextResponse.json({ user: newUserWithoutPassword, message: "User successfully created"}, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+    }
+}
+
 
 export async function GET(request: NextRequest) {
 
