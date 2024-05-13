32badd4eeb38de0e2ec3f64cc138d83e9288b83e
tamadam <tamadam9@gmail.com>
2024-05-13 18:20:11 +0200

    Add mobile friendly functions

--- app/(shop)/(marketplace)/components/ImageSlider/ImageSlider.tsx
+++ app/(shop)/(marketplace)/components/ImageSlider/ImageSlider.tsx
@@ -5,8 +5,9 @@ import { MdArrowForwardIos } from "react-icons/md";
 import { MdArrowBackIosNew } from "react-icons/md";
 import Button from "@/app/components/Button/Button";
 import { GoDotFill } from "react-icons/go";
-import { useState } from "react";
-import Image from "next/image";
+import { TouchEventHandler, useState } from "react";
+
+const SWIPE_TRESHOLD = 50;
 
 interface ImageSliderProps {
   imageUrls: string[];
@@ -14,14 +15,52 @@ interface ImageSliderProps {
 
 const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
   const [imageIndex, setImageIndex] = useState(0);
+  const [touchStart, setTouchStart] = useState<number | null>(null);
+  const [touchEnd, setTouchEnd] = useState<number | null>(null);
+
+  const handleLeftClick = () => {
+    setImageIndex((index) => (index === 0 ? imageUrls.length - 1 : index - 1));
+  };
+
+  const handleRightClick = () => {
+    setImageIndex((index) => (index === imageUrls.length - 1 ? 0 : index + 1));
+  };
+
+  const handleTouchStart: TouchEventHandler<HTMLDivElement> = (event) => {
+    setTouchEnd(0);
+    setTouchStart(event.touches[0].clientX);
+  };
+
+  const handleTouchMove: TouchEventHandler<HTMLDivElement> = (event) => {
+    setTouchEnd(event.touches[0].clientX);
+  };
+
+  const handleTouchEnd: TouchEventHandler<HTMLDivElement> = (event) => {
+    if (!touchStart || !touchEnd) return;
+
+    const distance = touchEnd - touchStart;
+
+    if (distance > -SWIPE_TRESHOLD) {
+      handleLeftClick();
+    }
+
+    if (distance < SWIPE_TRESHOLD) {
+      handleRightClick();
+    }
+  };
 
   return (
-    <div className={styles.imageSliderWrapper}>
+    <div
+      className={styles.imageSliderWrapper}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className={styles.imageSlider}>
         {imageUrls.map((imageUrl, index) => {
           return (
-            <Image
-              fill
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
               key={imageUrl}
               src={imageUrl}
               alt="advert image"
@@ -40,21 +79,13 @@ const ImageSlider = ({ imageUrls }: ImageSliderProps) => {
             Icon={MdArrowBackIosNew}
             shape="circle"
             className={`${styles.imageSliderButton} ${styles.buttonLeft}`}
-            onClick={() =>
-              setImageIndex((index) =>
-                index === 0 ? imageUrls.length - 1 : index - 1
-              )
-            }
+            onClick={handleLeftClick}
           />
           <Button
             Icon={MdArrowForwardIos}
             shape="circle"
             className={`${styles.imageSliderButton} ${styles.buttonRight}`}
-            onClick={() =>
-              setImageIndex((index) =>
-                index === imageUrls.length - 1 ? 0 : index + 1
-              )
-            }
+            onClick={handleRightClick}
           />
           <div className={styles.imageNavigation}>
             {imageUrls.map((_, index) => {
