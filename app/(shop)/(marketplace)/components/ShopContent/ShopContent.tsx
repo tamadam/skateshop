1a91ed115143fbcdc163b090eeafc2c70a7b13a7
tamadam <tamadam9@gmail.com>
2024-05-16 14:12:05 +0200

    Add basic products page

--- app/(shop)/(marketplace)/components/ShopContent/ShopContent.tsx
+++ app/(shop)/(marketplace)/components/ShopContent/ShopContent.tsx
@@ -2,11 +2,7 @@ import { PropsWithChildren } from "react";
 import styles from "./ShopContent.module.css";
 
 const ShopContent = ({ children }: PropsWithChildren) => {
-  return (
-    <div className={styles.shopContentWrapper}>
-      <div className={styles.shopContent}>{children}</div>
-    </div>
-  );
+  return <div className={styles.shopContentWrapper}>{children}</div>;
 };
 
 export default ShopContent;
