e8bcddc539de1d3f9e68f4cc61ea744b361fd5ff
tamadam <tamadam9@gmail.com>
2024-07-10 16:13:49 +0200

    Set header with new operator

--- app/(shop)/actions/getOrder.tsx
+++ app/(shop)/actions/getOrder.tsx
@@ -6,7 +6,7 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/orders`;
 const getOrder = async (orderId: string): Promise<OrderType> => {
   const res = await fetch(`${URL}/${orderId}`, {
     method: "GET",
-    headers: headers(),
+    headers: new Headers(headers()),
   });
 
   return res.json();
--- app/(shop)/actions/getOrders.tsx
+++ app/(shop)/actions/getOrders.tsx
@@ -6,7 +6,7 @@ const URL = `${process.env.NEXT_PUBLIC_API_URL}/orders`;
 const getOrders = async (): Promise<OrderType[]> => {
   const res = await fetch(URL, {
     method: "GET",
-    headers: headers(),
+    headers: new Headers(headers()),
   });
 
   return res.json();
